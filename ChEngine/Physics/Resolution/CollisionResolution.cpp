74642ec1f61eb518acb1e66a857be3a09cd21bff
Chunze Chen <cchen55@cchen55-c02t531lgtdy.attlocal.net>
2019-03-04 21:43:52 -0800

    Added comments from mac.

--- ChEngine/Physics/Resolution/CollisionResolution.cpp
+++ ChEngine/Physics/Resolution/CollisionResolution.cpp
@@ -13,6 +13,10 @@ void CollisionResolution::RunCollisionResolution()
 	while (!m_PhysicsManager->Collisions.empty())
 	{
 		CollisionInfo collision = m_PhysicsManager->Collisions.front();
+
+		// Resolve interpenetration, which will update the position of same rigid body
+		// in all other contacts
+
 		collision.Resolve();
 		m_PhysicsManager->Collisions.pop();
 	}
